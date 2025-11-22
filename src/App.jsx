78d4b97d83683aa74fabf534d18f72211f3c1f8e
Guillermomedinamartinez22 <guillermomedinamartinez01@gmail.com>
2025-11-22 12:17:22 -0600

    MI primer git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function Boton(){
+    return(
+        <button>
+            Soy un botón
+        </button>
+    );
+    
+}
+
+export default function miApp(){
+    return(
+        <div>
+            <h1>Bienvenidos a mi primera página web</h1><Boton />
+        </div>
+    );
+}
+
+
---
+++ src/Login/login.jsx
@@ -0,0 +1,16 @@
+function Boton(){
+    return(
+        <button>
+            Soy un botón
+        </button>
+    );
+    
+}
+
+export default function Mi_App(){
+    return(
+        <div>
+            <h1>Bienvenidos a mi primera página web</h1><Boton />
+        </div>
+    );
+}
