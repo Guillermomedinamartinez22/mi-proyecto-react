b272e21c0f77015cdd82a32017accc16b68c15ca
Guillermomedinamartinez22 <guillermomedinamartinez01@gmail.com>
2025-12-10 19:07:43 -0600

    última actualización

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-function Boton(){
-    return(
-        <button>
-            Soy un botón
-        </button>
-    );
+import './App.css'
+
+
+import React, { useState } from "react";
+
+function Boton({correo, pass}) {
+  const [mostrar, setMostrar] = useState(false);
+
+  function mensaje() {
+    if(correo.trim()!== '' && pass.trim()!== ''){
+        setMostrar(true); // cambia el estado a true
+    } else{
+        alert('Por favor ingrese correo y contraseña correctamente')
+    }
     
+  }
+
+  return (
+    <div>
+      <button className='boton' type="button" onClick={mensaje}>
+        Ingresar
+      </button>
+    
+      {mostrar && <h1>¡Bienvenido!</h1>}
+    </div>
+  );
 }
 
+
 export default function miApp(){
+    const [correo, setCorreo] = useState('');
+    const [pass, setPass] = useState('');
+
     return(
         <div>
-            <h1>Bienvenidos a mi primera página web</h1><Boton />
+            <h1>Bienvenido</h1>
+            <h1>Ingresa tu cuenta</h1>
+            <div className='contenedor'>
+                <p>Ingresar correo electrónico</p>
+                <input id='correo' value={correo} type="email" placeholder='Correo Electrónico' onChange={(e) => setCorreo(e.target.value)} required />
+                <p>Ingresar contraseña</p>
+                <input id='pass' value={pass} type="password" placeholder='Contraseña' onChange={(e) => setPass(e.target.value)} required/>
+
+            </div>
+            <Boton correo={correo} pass={pass}/>
         </div>
     );
 }
